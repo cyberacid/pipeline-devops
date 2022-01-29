9e8ef6d7d4f8ea2e388d76a45cc88cd45b26e6a2
Fernando Peña <fpenah@gmail.com>
2022-01-29 11:27:54 -0300

    Fix

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -5,31 +5,31 @@
 */
 def call(){
   stage("Paso 1: Compliar"){
-      env.TAREA = env.STAGE_NAME
+      TAREA = env.STAGE_NAME
       sh "mvn clean compile -e"
   }
   stage("Paso 2: Testear"){
-      env.TAREA = env.STAGE_NAME
+      TAREA = env.STAGE_NAME
       sh "mvn clean test -e"
   }
   stage("Paso 3: Build .Jar"){
-      env.TAREA = env.STAGE_NAME
+      TAREA = env.STAGE_NAME
       sh "mvn clean package -e"
   }
   stage("Paso 4: Sonar - Análisis Estático"){
-      env.TAREA = env.STAGE_NAME
+      TAREA = env.STAGE_NAME
       sh "echo 'Análisis Estático!'"
       withSonarQubeEnv('sonarqube') {
           sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
       }
   }
   stage("Paso 5: Curl Springboot Gradle sleep 20"){
-      env.TAREA = env.STAGE_NAME
+      TAREA = env.STAGE_NAME
       sh "gradle bootRun&"
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
   stage("Paso 6: Subir Nexus"){
-      env.TAREA = env.STAGE_NAME
+      TAREA = env.STAGE_NAME
       nexusPublisher nexusInstanceId: 'nexus',
       nexusRepositoryId: 'devops-usach-nexus',
       packages: [
@@ -50,15 +50,15 @@ def call(){
       ]
   }
   stage("Paso 7: Descargar Nexus"){
-      env.TAREA = env.STAGE_NAME
+      TAREA = env.STAGE_NAME
       sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
   }
   stage("Paso 8: Levantar Artefacto Jar"){
-      env.TAREA = env.STAGE_NAME
+      TAREA = env.STAGE_NAME
       sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
   }
   stage("Paso 9: Testear Artefacto - Dormir(Esperar 20sg) "){
-      env.TAREA = env.STAGE_NAME
+      TAREA = env.STAGE_NAME
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
 }
